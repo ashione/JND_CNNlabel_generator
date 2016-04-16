04a03f5b162b87be9c3ff83e09cfac0f1b871939
ashione <skyzlxuan@gmail.com>
2016-04-16 13:02:33 +0800

    counting SQF data

---
+++ JND.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+class JNDLable:
+
+    def __init__(self,fileName='../SQF_data/Image_%d_l_h.txt',fileNums=50):
+        self.fileName = fileName
+        self.fileNums = fileNums
+        self.generate()
+
+    def readFile(self,fileName):
+        tempFile = open(fileName)
+        lines = map(lambda x : filter(lambda f : f!='',x.replace('\r','').replace('\n','').split(' ')),tempFile.readlines())
+        return map(lambda x : (int(x[0]),float( x[1] )),zip(lines[0],lines[1]))
+
+    def generate(self):
+        self.jnd = []
+        self.maxJND_level = 0
+        for i in range(1,self.fileNums+1):
+            tempFileName = self.fileName.replace('%d',str(i))
+            self.jnd.append( self.readFile(tempFileName))
+            self.maxJND_level = max(self.maxJND_level,len(self.jnd[len(self.jnd)-1]))
+
+
+
+if __name__ == '__main__':
+    jnd = JNDLable()
+    print jnd.maxJND_level
+
+
